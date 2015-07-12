2df469a5f520dc72da036da1831a438027a72c60
brycearden <arden.bryce@gmail.com>
2015-07-12 09:43:55 -0500

    add these files to Docmuments/Energia/libraries/Event

---
+++ Event.h
@@ -0,0 +1,15 @@
+#include <Energia.h>
+#include <xdc/runtime/Error.h>
+#include <ti/sysbios/knl/Event.h>
+#include <ti/sysbios/BIOS.h>
+
+class Event {
+    private:
+        Event_Handle eventHandle;
+        static xdc_UInt eventId;
+    public:
+        Event();
+        void begin();
+        void send();
+        void waitFor();
+};
