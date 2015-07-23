c620b3948caaf43bd927aa58c187cd77f7160849
Bryce Arden <brycearden@Bryces-MacBook-Pro.local>
2015-07-22 23:30:47 -0500

    added some comments for PWM

--- myWifiSensor/PWM.ino
+++ myWifiSensor/PWM.ino
@@ -2,16 +2,18 @@
  * Setup for the pwm function for output to the electrochromic film
  */
 
-
 int PWM_PIN = 38;
 
 void setup() {
   pinMode(PWM_PIN, OUTPUT);
-//  Serial.begin(115200);
 }
+
+/*
+ * use PWM to control the MOSfet transistor for the electrochromic film
+ * analogWrite generates PWM with dutycyle = outputVal and f ~ 500Hz
+ */
 
 void loop() {
   analogWrite(PWM_PIN, outputVal);
-  //Serial.println("wrote to smart window!");
   delay(100);
 }
